4629d4cbe15ad6d181bccd938499c81b7115183f
Adithya Sakaray <adithyasakaray@gmail.com>
2024-03-17 03:51:08 +0530

    Upload post added

--- lib/Backend_services/post_manager.dart
+++ lib/Backend_services/post_manager.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:http/http.dart' as http;
 
 
@@ -19,6 +20,34 @@ class PostsManager {
       throw Exception('Failed to load posts');
     }
   }
+
+  static Future<String?> uploadPost(String userid, String content) async {
+    String url = "https://devhousebackend.vercel.app/post/post";
+    var body = {
+      'userId': userid,
+      'content': content,
+      'type': 'student'
+    };
+
+    var response = await http.post(
+      Uri.parse(url),
+      body: body
+    );
+
+    if (response.statusCode == 201) {
+      print('POST request successful');
+      print('Response body: ${response.body}');
+      Map<String, dynamic> jsonData = json.decode(response.body);
+      print("PostID: ${jsonData["postId"]}");
+
+      return jsonData["postId"];
+
+    } else {
+      print('Failed to make POST request. Status code: ${response.statusCode}');
+    }
+  }
+
+
 
 }
 
